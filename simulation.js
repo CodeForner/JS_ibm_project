fb31b03eae0492eed48594e1e73dfd6dda2f2e79
Eric Garcia <ericgforner@gmail.com>
2024-07-10 15:05:44 +0200

    Corrected and changed name variable on the Name field (recommendation section) .

--- simulation.js
+++ simulation.js
@@ -6,33 +6,58 @@ button.addEventListener('click', function() {
   const simular = document.getElementsByClassName('simular')[0];
 
   if (button.textContent === 'Start') {
-    // Change to the active state
+    
 
     staticImage.style.zIndex = -1;
     gifImage.style.opacity = 1;
     staticImage.style.opacity = 0;
     button.textContent = 'Stop';
 
-    // Restart the GIF
+    //Reseteamos el GIF
     const gifSrc = gifImage.src;
     gifImage.src = '';
     gifImage.src = gifSrc;
   } else {
-    // Reset to the initial state
+    //Reseteamos al estado inicial
     gifImage.style.zIndex = 2;
     staticImage.style.opacity = 1;
     gifImage.style.opacity = 0;
     button.textContent = 'Start';
 
-    // Restart the GIF (if needed, else leave it as is)
+    
     const gifSrc = gifImage.src;
     gifImage.src = '';
     gifImage.src = gifSrc;
   }
 });
 
-const submit = document.getElementById('submit');
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('submit').addEventListener('click', function() {
+      let nameInput = document.getElementById("name2").value;
+      let messageInput = document.getElementById('message').value;
+      
+      //Comprobamos que haya puesto el nombre
+      if (nameInput === "") {
+        alert("Name is required to submit a recommendation.");
+        return;
+    }
 
-submit.addEventListener('click', function() {
-  window.alert("You have submitted a recommendation!");
+      //Creamos una nueva recomendación.
+      const recommendationDiv = document.createElement('div');
+      //Aquí escribimos el contenido directamente, como si fuera una string
+      const recommendationContent = `
+          ${messageInput ? messageInput : 'No message provided.'}
+          <span class="author">Author: ${nameInput}</span>
+      `;
+      recommendationDiv.innerHTML = recommendationContent;
+
+      //
+      document.querySelector('.reco').appendChild(recommendationDiv);
+
+      //Limpiamos el contenido del elemento para reiniciar el estado
+      document.getElementById('name2').value = '';
+      document.getElementById('message').value = '';
+  });
 });
+
+
